f8c964358aef5791f24e1eed590506e41435ce0d
Saveliy <teplenichev.dev@gmail.com>
2026-09-21 09:46:43 +0300

    feat: реализовано сохранение задач в localStorage

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 // Состояние
-const state = [
-    {
-        id: 1789803718527,
-        text: "Купить молоко",
-        completed: false,         
-    }
+let state = [
+
 ]
+
+
+function loadState(key, fallback) {
+    // state = JSON.parse( localStorage.getItem('todo-list'))
+    
+    try {
+        const raw = localStorage.getItem(key)
+        return raw ? JSON.parse(raw) : fallback
+    } catch(e) {
+        console.warn('Ошибка чтения из localStorage:', e)
+        return fallback
+    }
+
+
+}
+
+function saveState() {
+    const str = JSON.stringify(state)
+    localStorage.setItem('todo-list', str)
+}
+
 
 function addTask() {
     const form = document.getElementById('todo-form');
@@ -24,20 +41,28 @@ function addTask() {
             text: clean,
             completed: false,
         }
-
+        
         input.value = ""
        state.unshift(newTask)
+       saveState()
        renderList()
     })
 }
 
 function renderList() {
-
-
+    const empty = document.getElementById('empty-state')
     const list = document.getElementById('todo-list')
+    const counter = document.getElementById('counter-value')
     list.innerHTML = ""
 
-    state.forEach(task => {
+    if (state.length === 0) {
+        empty.style.display = 'flex'
+    }  else {
+        empty.style.display = 'none'
+    }
+
+
+     state.forEach(task => {
         const li = document.createElement('li')
         const span = document.createElement('span')
         const input = document.createElement('input')
@@ -67,8 +92,14 @@ function renderList() {
         // Можно так |
         li.append(input, span, button)
         list.appendChild(li)
+
+
+        const activeCount = state.filter(t => !t.completed).length
+        counter.textContent = activeCount
     })
 }
 
+
+state = loadState('todo-list', [])
 addTask()
 renderList()
