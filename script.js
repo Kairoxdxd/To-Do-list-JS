191fed74c016859f584f1fcff134c4941f87848d
Saveliy <teplenichev.dev@gmail.com>
2026-09-23 09:21:35 +0300

    feat: закончен todo-list с поиском и фильтрами

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ let state = [
 
 ]
 
+// Поиск
+let searchQuery = ''
 
+// Фильтр
+let filter = "all"
+
+// Ключ localStorage
+const STORAGE_KEY = 'todo-list'
+
+
+// LocalStorage
 function loadState(key, fallback) {
     // state = JSON.parse( localStorage.getItem('todo-list'))
     
@@ -18,16 +28,16 @@ function loadState(key, fallback) {
 
 }
 
-function saveState() {
+function saveState(key) {
     const str = JSON.stringify(state)
-    localStorage.setItem('todo-list', str)
+    localStorage.setItem(key, str)
 }
 
 
+// Настройки формы добавления
 function addTask() {
     const form = document.getElementById('todo-form');
     const input = document.getElementById('todo-input');
-    const list = document.getElementById('todo-list')
     
     form.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -44,25 +54,69 @@ function addTask() {
         
         input.value = ""
        state.unshift(newTask)
-       saveState()
+       saveState(STORAGE_KEY)
        renderList()
     })
 }
 
+
+// Настройка поиска
+function setupSearch() {
+    const input = document.getElementById('search-input');
+
+    input.addEventListener('input', (e) => {
+        searchQuery = e.target.value.toLowerCase()
+        renderList()
+    })
+}
+
+function setupFilters() {
+    const buttons = document.querySelectorAll('.app__filter');
+
+    buttons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            filter = btn.dataset.filter
+
+            buttons.forEach(b => b.classList.remove("is-active"))
+            btn.classList.add('is-active')
+            renderList()
+        })
+    })
+}
+
+// Возвращает задачи после фильтра и поиска
+function getFilteredTasks() {
+    let tasks = state
+    if (searchQuery !== '') {
+       tasks = tasks.filter(t => t.text.toLowerCase().includes(searchQuery))
+    }
+
+    if (filter === "active") {
+        tasks = tasks.filter(t => !t.completed)
+    }
+
+    if (filter === "completed") {
+        tasks = tasks.filter(t =>  t.completed)
+    }
+    return tasks
+}
+
+// Рендеринг задач
 function renderList() {
+    const tasks = getFilteredTasks();
     const empty = document.getElementById('empty-state')
     const list = document.getElementById('todo-list')
     const counter = document.getElementById('counter-value')
     list.innerHTML = ""
 
-    if (state.length === 0) {
+    if (tasks.length === 0) {
         empty.style.display = 'flex'
     }  else {
         empty.style.display = 'none'
     }
 
 
-     state.forEach(task => {
+     tasks.forEach(task => {
         const li = document.createElement('li')
         const span = document.createElement('span')
         const input = document.createElement('input')
@@ -93,13 +147,41 @@ function renderList() {
         li.append(input, span, button)
         list.appendChild(li)
 
+        input.addEventListener("click", () => {
+           task.completed = !task.completed;
+           saveState(STORAGE_KEY)
+           renderList()
+        })
 
+        if (task.completed) {
+            span.style.textDecoration = "line-through"
+        }
+
+        button.addEventListener('click', () => {
+            state = state.filter(t => t.id !== task.id)
+            saveState(STORAGE_KEY)
+            renderList()
+        })
+    })
         const activeCount = state.filter(t => !t.completed).length
         counter.textContent = activeCount
-    })
+
+        
+
+
 }
+// Удалить ВСЕ выполненные
+  document.getElementById('clear-done').addEventListener('click', () => {
+           state = state.filter(t => !t.completed)
+            saveState(STORAGE_KEY)
+            renderList()
+        })
 
 
-state = loadState('todo-list', [])
+state = loadState(STORAGE_KEY, [])
+setupFilters()
+setupSearch()
 addTask()
 renderList()
+
+
